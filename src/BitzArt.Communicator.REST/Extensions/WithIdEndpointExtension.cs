50c76472e80c9cb07015b99b013bb939ae88f4a4
Yuriy Durov <u.a.durov@gmail.com>
2023-09-13 15:31:59 +0400

    Split WithEndpoint/WithIdEndpoint extensions

--- src/BitzArt.Communicator.REST/Extensions/WithIdEndpointExtension.cs
+++ src/BitzArt.Communicator.REST/Extensions/WithIdEndpointExtension.cs
@@ -1,23 +1,7 @@
 ﻿namespace BitzArt.Communicator;
 
-public static class ICommunicatorRestEntityBuilderExtensions
+public static class WithIdEndpointExtension
 {
-    public static ICommunicatorRestEntityBuilder<TEntity> WithEndpoint<TEntity>(this ICommunicatorRestEntityBuilder<TEntity> builder, string endpoint)
-        where TEntity : class
-    {
-        builder.EntityOptions.Endpoint = endpoint;
-
-        return builder;
-    }
-
-    public static ICommunicatorRestEntityBuilder<TEntity, TKey> WithEndpoint<TEntity, TKey>(this ICommunicatorRestEntityBuilder<TEntity, TKey> builder, string endpoint)
-        where TEntity : class
-    {
-        builder.EntityOptions.Endpoint = endpoint;
-
-        return builder;
-    }
-
     public static ICommunicatorRestEntityBuilder<TEntity> WithIdEndpoint<TEntity>(this ICommunicatorRestEntityBuilder<TEntity> builder, string endpoint)
         where TEntity : class
     {
