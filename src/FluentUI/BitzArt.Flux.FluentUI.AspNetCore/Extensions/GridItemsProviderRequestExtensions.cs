a1a1e6290155fdfe85cf791d91e180e099c10d0f
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2024-05-15 09:57:20 +0400

    Update GetSorting method

--- src/FluentUI/BitzArt.Flux.FluentUI.AspNetCore/Extensions/GridItemsProviderRequestExtensions.cs
+++ src/FluentUI/BitzArt.Flux.FluentUI.AspNetCore/Extensions/GridItemsProviderRequestExtensions.cs
@@ -6,16 +6,21 @@ namespace BitzArt.Flux;
 
 public static class GridItemsProviderRequestExtensions
 {
-    public static ValueTuple<LambdaExpression, bool>? GetSorting<TModel>(this GridItemsProviderRequest<TModel> request)
+    public static ValueTuple<Expression, bool>? GetSorting<TModel>(this GridItemsProviderRequest<TModel> request)
     {
         var sortBy = request.SortByColumn?.SortBy;
         var type = sortBy?.GetType();
         var fieldInfo = type?.GetField("_firstExpression", BindingFlags.NonPublic | BindingFlags.Instance);
         var sorting = fieldInfo?.GetValue(sortBy) as ValueTuple<LambdaExpression, bool>?;
 
-        return sorting;
+        if (!sorting.HasValue) return null;
+
+        var sortingExpression = sorting.Value.Item1!.Body;
+        var isAscending = sorting.Value.Item2;
+
+        return (sortingExpression, isAscending);
     }
 
-    public static LambdaExpression? GetSortingExpression<TModel>(this GridItemsProviderRequest<TModel> request)
+    public static Expression? GetSortingExpression<TModel>(this GridItemsProviderRequest<TModel> request)
         => request.GetSorting()?.Item1;
 }
