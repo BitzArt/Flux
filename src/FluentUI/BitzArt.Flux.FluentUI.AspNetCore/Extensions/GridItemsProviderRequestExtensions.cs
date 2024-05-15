2d906262828de2c9630af1ee8860c2bb5e6f29a6
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2024-05-15 09:50:49 +0400

    Add GridItemsProviderRequestExtensions

---
+++ src/FluentUI/BitzArt.Flux.FluentUI.AspNetCore/Extensions/GridItemsProviderRequestExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.FluentUI.AspNetCore.Components;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace BitzArt.Flux;
+
+public static class GridItemsProviderRequestExtensions
+{
+    public static ValueTuple<LambdaExpression, bool>? GetSorting<TModel>(this GridItemsProviderRequest<TModel> request)
+    {
+        var sortBy = request.SortByColumn?.SortBy;
+        var type = sortBy?.GetType();
+        var fieldInfo = type?.GetField("_firstExpression", BindingFlags.NonPublic | BindingFlags.Instance);
+        var sorting = fieldInfo?.GetValue(sortBy) as ValueTuple<LambdaExpression, bool>?;
+
+        return sorting;
+    }
+
+    public static LambdaExpression? GetSortingExpression<TModel>(this GridItemsProviderRequest<TModel> request)
+        => request.GetSorting()?.Item1;
+}
