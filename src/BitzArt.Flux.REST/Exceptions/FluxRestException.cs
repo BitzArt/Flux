939ac20a9684a3c37489044fe309814807e77711
Yuriy Durov <u.a.durov@gmail.com>
2024-07-29 10:52:12 +0400

    Clearer exceptions on http request handling errors

---
+++ src/BitzArt.Flux.REST/Exceptions/FluxRestException.cs
@@ -0,0 +1,20 @@
+﻿namespace BitzArt.Flux;
+
+/// <summary>
+/// A exception thrown when a generic error occurs in Flux.REST.
+/// </summary>
+public class FluxRestException : Exception
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FluxRestException"/> class.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="innerException"></param>
+    public FluxRestException(string message, Exception innerException) : base(message, innerException) { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FluxRestException"/> class.
+    /// </summary>
+    /// <param name="message"></param>
+    public FluxRestException(string message) : base(message) { }
+}
---
+++ src/BitzArt.Flux.REST/Exceptions/FluxRestRequestHandlingException.cs
@@ -0,0 +1,20 @@
+﻿namespace BitzArt.Flux;
+
+/// <summary>
+/// An exception thrown when an error occurs during the request handling.
+/// </summary>
+public class FluxRestRequestHandlingException : FluxRestException
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FluxRestRequestHandlingException"/> class.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="innerException"></param>
+    public FluxRestRequestHandlingException(string message, Exception innerException) : base(message, innerException) { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FluxRestRequestHandlingException"/> class.
+    /// </summary>
+    /// <param name="message"></param>
+    public FluxRestRequestHandlingException(string message) : base(message) { }
+}
