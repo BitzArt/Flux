2545c28e39c0bab0d1a7db4261d78f3a98ea65a0
Yuriy Durov <u.a.durov@gmail.com>
2024-09-24 18:07:02 +0400

    Let's try a different approach to project structure

---
+++ src/REST/BitzArt.Flux.REST/Interfaces/IFluxRestSetEndpointBuilder.cs
@@ -0,0 +1,11 @@
+﻿namespace BitzArt.Flux;
+
+public interface IFluxRestSetEndpointBuilder<TModel> : IFluxRestSetBuilder<TModel>
+    where TModel : class
+{
+}
+
+public interface IFluxRestSetEndpointBuilder<TModel, TKey> : IFluxRestSetEndpointBuilder<TModel>
+    where TModel : class
+{
+}
