72b328b6371a2adf3d3be4de515845b130f773f9
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2024-10-29 19:38:56 +0400

    Update MudTableSortSelector

--- src/MudBlazor/BitzArt.Flux.MudBlazor/Components/MudTableSortSelector.razor.cs
+++ src/MudBlazor/BitzArt.Flux.MudBlazor/Components/MudTableSortSelector.razor.cs
@@ -2,136 +2,62 @@
 
 namespace MudBlazor;
 
-/// <summary>
-/// 
-/// </summary>
-/// <typeparam name="T"></typeparam>
 public partial class MudTableSortSelector<T>
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    [Parameter]
-    public IEnumerable<MudTableSortLabel<T>>? SortLabels { get; set; }
+    private ICollection<MudTableSortSelectorItem<T>> _items = [];
 
-    /// <summary>
-    /// 
-    /// </summary>
     [Parameter, EditorRequired]
     public required RenderFragment ChildContent { get; set; }
 
-
-    public MudTableSortLabel<T>? CurrentSortLabel 
-    {
-        get => _currentSortLabel; 
-        set => _currentSortLabel = value;
-    }
-    
-    private MudTableSortLabel<T>? _currentSortLabel;
-
-
-
-    private SortDirection _currentSortDirection;
-
-
-
-    /// <summary>
-    /// 
-    /// </summary>
     [Parameter]
-    public string AscendingSortIcon { get; set; } = Icons.Material.Filled.ArrowUpward;
+    public EventCallback<MudTableSortSelectorItem<T>> ValueChanged { get; set; }
 
-    /// <summary>
-    /// 
-    /// </summary>
     [Parameter]
-    public string DescendingSortIcon { get; set; } = Icons.Material.Filled.ArrowDownward;
+    public EventCallback<MudTableSortLabel<T>> SortChanged { get; set; }
 
-    private string _currentSortIcon => _currentSortDirection switch
-    {
-        SortDirection.Ascending => AscendingSortIcon,
-        SortDirection.Descending => DescendingSortIcon,
-        SortDirection.None => AscendingSortIcon, // TODO: Handle this case. Consider that default sorting may be descending.
-        _ => throw new InvalidOperationException($"Invalid sort direction: {_currentSortDirection}.")
-    };
-
-    /// <summary>
-    /// 
-    /// </summary>
     [Parameter]
-    public Variant Variant { get; set; } = Variant.Text;
+    public MudTable<T>? Table { get; set; }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    [Parameter]
-    public Margin Margin { get; set; } = Margin.Normal;
+    private MudSelect<MudTableSortSelectorItem<T>> _select;
 
+    public SortDirection? SortDirection { get; set; }
 
-    private void OnSortingChanged(MudTableSortLabel<T>? sortLabel, SortDirection? sortDirection)
-    {
-        _currentSortLabel = sortLabel;
+    public MudTableSortSelectorItem<T>? SelectedItem 
+    { 
+        get => _selectedItem; 
+        set
+        {
+            _selectedItem = value;
+            _ = OnValueChangedAsync(value);
+        }
     }
 
+    private MudTableSortSelectorItem<T>? _selectedItem;
 
+    public void AddItem(MudTableSortSelectorItem<T> item)
+    {
+        _items.Add(item);
+    }
 
+    private async Task OnValueChangedAsync(MudTableSortSelectorItem<T>? item)
+    {
+        //_selectedItem = item;
 
-    //[Parameter]
-    //public RenderFragment? ChildContent { get; set; }
+        if (item is null)
+            return;
 
-    //private SortDirection? _currentSortDirection
-    //{
-    //    get
-    //    {
-    //        if (_currentSortLabel is null || _currentSortLabel.SortDirection == SortDirection.None) 
-    //            return null;
+        if (item.SortDirection.HasValue)
+            SortDirection = item.SortDirection;
 
-    //        return _currentSortLabel.SortDirection;
-    //    }
-    //}
+        if (ValueChanged.HasDelegate)
+            await ValueChanged.InvokeAsync(item);
 
-    //[SuppressMessage("Usage", "BL0005:Component parameter should not be set outside of its component.")]
-    //private async Task ChangeSortByAsync(MudTableSortLabel<T>? sortLabel)
-    //{
-    //    if (_currentSortLabel == sortLabel) return;
+        var sortLabel = item.GetSortLabel();
 
-    //    if (Table is null) throw new InvalidOperationException(
-    //       "Table component must be forwarded to MudTableSortSelector for it to be able to set sorting.");
+        if (SortChanged.HasDelegate)
+            await SortChanged.InvokeAsync(sortLabel);
 
-    //    foreach (var label in Table.Context.SortLabels)
-    //    {
-    //        label.SortDirection = SortDirection.None;
-    //    }
-
-    //    if (sortLabel is null)
-    //    {
-    //        Table.Context.CurrentSortLabel!.SortDirection = SortDirection.None;
-    //        await Table.Context.SetSortFunc(Table.Context.CurrentSortLabel).IgnoreCancellation();
-    //    }
-    //    else
-    //    {
-    //        sortLabel.SortDirection = _currentSortDirection.HasValue ? _currentSortDirection.Value : SortDirection.Ascending;
-    //        await Table.Context.SetSortFunc(sortLabel).IgnoreCancellation();
-    //    }
-    //}
-
-    //[SuppressMessage("Usage", "BL0005:Component parameter should not be set outside of its component.")]
-    //private async Task ChangeSortDirtectionAsync(SortDirection? sortDirection)
-    //{
-    //    if (_currentSortDirection == sortDirection) return;
-
-    //    if (Table is null) throw new InvalidOperationException(
-    //       "Table component must be forwarded to MudTableSortSelector for it to be able to set sorting.");
-
-    //    foreach (var sortLabel in Table.Context.SortLabels)
-    //    {
-    //        sortLabel.SortDirection = SortDirection.None;
-    //    }
-
-    //    if (Table.Context.CurrentSortLabel is not null)
-    //    {
-    //        Table.Context.CurrentSortLabel.SortDirection = sortDirection.HasValue ? sortDirection.Value : SortDirection.None;
-    //        await Table.Context.SetSortFunc(Table.Context.CurrentSortLabel).IgnoreCancellation();
-    //    }
-    //}
+        if (Table is not null)
+            await Table.Context.SetSortFunc(sortLabel);
+    }
 }
---
+++ src/MudBlazor/BitzArt.Flux.MudBlazor/Components/MudTableSortSelectorItem.razor.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MudBlazor;
+
+public partial class MudTableSortSelectorItem<T>
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter, EditorRequired]
+    public required RenderFragment ChildContent { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    public string? SortLabel { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    public SortDirection? SortDirection { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [CascadingParameter]
+    private MudTableSortSelector<T>? ParentSelector { get; set; } = null!;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        ParentSelector?.AddItem(this);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public MudTableSortLabel<T> GetSortLabel()
+    {
+        return new MudTableSortLabel<T>
+        {
+            SortLabel = SortLabel,
+            SortDirection = GetSortDirection()
+        };
+    }
+
+    private SortDirection GetSortDirection()
+    {
+        if (SortDirection.HasValue)
+            return SortDirection.Value;
+
+        if (ParentSelector is not null && ParentSelector.SortDirection.HasValue)
+            return ParentSelector.SortDirection.Value;
+
+        return MudBlazor.SortDirection.None;
+    }
+}
