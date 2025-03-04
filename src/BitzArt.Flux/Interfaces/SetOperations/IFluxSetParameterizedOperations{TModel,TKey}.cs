225f84395b2b8adecc725f2dbbfc6c916d02e07a
Yuriy Durov <u.a.durov@gmail.com>
2025-03-04 14:43:50 +0400

    Split operation interface files by generic definitions

---
+++ src/BitzArt.Flux/Interfaces/SetOperations/IFluxSetParameterizedOperations{TModel,TKey}.cs
@@ -0,0 +1,43 @@
+﻿namespace BitzArt.Flux;
+
+/// <summary>
+/// Parametrized operations for <see href="https://bitzart.github.io/Flux/">Flux</see> Sets.
+/// </summary>
+/// <typeparam name="TModel">Model type of the set.</typeparam>
+/// <typeparam name="TKey">Key type of the set.</typeparam>
+public interface IFluxSetParameterizedOperations<TModel, TKey>
+{
+    // ============================== GetAsync ==============================
+
+    /// <summary>
+    /// Fetches an object from the set.
+    /// </summary>
+    /// <typeparam name="TInputParameters">Input parameters type.</typeparam>
+    /// <param name="id">Unique identifier of the object to fetch.</param>
+    /// <param name="parameters">Parameters used by the operation.</param>
+    /// <param name="cancellationToken">Cancellation token for this operation.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    public Task<TModel> GetAsync<TInputParameters>(TKey id, TInputParameters parameters, CancellationToken cancellationToken = default)
+        where TInputParameters : notnull, IRequestParameters;
+
+    // ============================== UpdateAsync ==============================
+
+    /// <inheritdoc cref="UpdateAsync{TInputParameters, TResponse}(TKey, TModel, TInputParameters, bool, CancellationToken)"/>
+    public Task<TModel> UpdateAsync<TInputParameters>(TKey id, TModel model, TInputParameters parameters, bool partial = false, CancellationToken cancellationToken = default)
+        where TInputParameters : notnull, IRequestParameters
+        => UpdateAsync<TInputParameters, TModel>(id, model, parameters, partial, cancellationToken);
+
+    /// <summary>
+    /// Updates an existing object in the set.
+    /// </summary>
+    /// <typeparam name="TInputParameters">Input parameters type.</typeparam>
+    /// <typeparam name="TResponse">Response type.</typeparam>
+    /// <param name="id">Unique identifier of the object to update.</param>
+    /// <param name="value">The value to update the object with.</param>
+    /// <param name="parameters">Parameters used by the operation.</param>
+    /// <param name="partial">Whether to perform a partial update (e.g. PATCH in REST).</param>
+    /// <param name="cancellationToken">Cancellation token for this operation.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    public Task<TResponse> UpdateAsync<TInputParameters, TResponse>(TKey id, TModel value, TInputParameters parameters, bool partial = false, CancellationToken cancellationToken = default)
+        where TInputParameters : notnull, IRequestParameters;
+}
---
+++ src/BitzArt.Flux/Interfaces/SetOperations/IFluxSetParameterlessOperations{TModel,TKey}.cs
@@ -0,0 +1,23 @@
+﻿namespace BitzArt.Flux;
+
+/// <summary>
+/// Parameterless operations for <see href="https://bitzart.github.io/Flux/">Flux</see> Sets.
+/// </summary>
+/// <typeparam name="TModel">Model type of the set.</typeparam>
+/// <typeparam name="TKey">Key type of the set.</typeparam>
+public interface IFluxSetParameterlessOperations<TModel, TKey>
+{
+    // ============================== GetAsync ==============================
+
+    /// <inheritdoc cref="IFluxSetParameterizedOperations{TModel,TKey}.GetAsync{TInputParameters}(TKey, TInputParameters, CancellationToken)"/>
+    public Task<TModel> GetAsync(TKey id, CancellationToken cancellationToken = default);
+
+    // ============================== UpdateAsync ==============================
+
+    /// <inheritdoc cref="IFluxSetParameterizedOperations{TModel,TKey}.UpdateAsync{TInputParameters, TResponse}(TKey, TModel, TInputParameters, bool, CancellationToken)"/>
+    public Task<TModel> UpdateAsync(TKey id, TModel model, bool partial = false, CancellationToken cancellationToken = default)
+        => UpdateAsync<TModel>(id, model, partial, cancellationToken);
+
+    /// <inheritdoc cref="IFluxSetParameterizedOperations{TModel,TKey}.UpdateAsync{TInputParameters, TResponse}(TKey, TModel, TInputParameters, bool, CancellationToken)"/>
+    public Task<TResponse> UpdateAsync<TResponse>(TKey id, TModel model, bool partial = false, CancellationToken cancellationToken = default);
+}
