48f0a95d1952edeb3c029e443485e0371b01acce
Yuriy Durov <yu.durov@gmail.com>
2025-05-15 18:16:27 +0400

    It's growing

--- src/REST/BitzArt.Flux.REST/Extensions/SetEndpoints/EndpointResolverExtensions.cs
+++ src/REST/BitzArt.Flux.REST/Extensions/SetEndpoints/EndpointResolverExtensions.cs
@@ -8,6 +8,46 @@ namespace BitzArt.Flux;
 /// </summary>
 public static class EndpointResolverExtensions
 {
+    // ==============================================================
+
+    /// <inheritdoc cref="WithEndpoint{TModel,TKey,TOperationDescriptor}(IFluxRestSetBuilder{TModel,TKey},Func{TOperationDescriptor,IServiceProvider,HttpRequestMessage},HttpMethods)"/>
+    public static IFluxRestSetBuilder<TModel, TKey> WithGet<TModel, TKey, TOperationDescriptor>(this IFluxRestSetBuilder<TModel, TKey> builder, Func<TOperationDescriptor, HttpRequestMessage> requestMessageResolver)
+        where TModel : class
+        where TOperationDescriptor : OperationDescriptor
+        => builder.WithEndpoint(requestMessageResolver, HttpMethods.Get);
+
+    /// <inheritdoc cref="WithEndpoint{TModel,TKey,TOperationDescriptor}(IFluxRestSetBuilder{TModel,TKey},Func{TOperationDescriptor,IServiceProvider,HttpRequestMessage},HttpMethods)"/>
+    public static IFluxRestSetBuilder<TModel, TKey> WithPost<TModel, TKey, TOperationDescriptor>(this IFluxRestSetBuilder<TModel, TKey> builder, Func<TOperationDescriptor, HttpRequestMessage> requestMessageResolver)
+        where TModel : class
+        where TOperationDescriptor : OperationDescriptor
+        => builder.WithEndpoint(requestMessageResolver, HttpMethods.Post);
+
+    /// <inheritdoc cref="WithEndpoint{TModel,TKey,TOperationDescriptor}(IFluxRestSetBuilder{TModel,TKey},Func{TOperationDescriptor,IServiceProvider,HttpRequestMessage},HttpMethods)"/>
+    public static IFluxRestSetBuilder<TModel, TKey> WithPut<TModel, TKey, TOperationDescriptor>(this IFluxRestSetBuilder<TModel, TKey> builder, Func<TOperationDescriptor, HttpRequestMessage> requestMessageResolver)
+        where TModel : class
+        where TOperationDescriptor : OperationDescriptor
+        => builder.WithEndpoint(requestMessageResolver, HttpMethods.Put);
+
+    /// <inheritdoc cref="WithEndpoint{TModel,TKey,TOperationDescriptor}(IFluxRestSetBuilder{TModel,TKey},Func{TOperationDescriptor,IServiceProvider,HttpRequestMessage},HttpMethods)"/>
+    public static IFluxRestSetBuilder<TModel, TKey> WithPatch<TModel, TKey, TOperationDescriptor>(this IFluxRestSetBuilder<TModel, TKey> builder, Func<TOperationDescriptor, HttpRequestMessage> requestMessageResolver)
+        where TModel : class
+        where TOperationDescriptor : OperationDescriptor
+        => builder.WithEndpoint(requestMessageResolver, HttpMethods.Patch);
+
+    /// <inheritdoc cref="WithEndpoint{TModel,TKey,TOperationDescriptor}(IFluxRestSetBuilder{TModel,TKey},Func{TOperationDescriptor,IServiceProvider,HttpRequestMessage},HttpMethods)"/>
+    public static IFluxRestSetBuilder<TModel, TKey> WithDelete<TModel, TKey, TOperationDescriptor>(this IFluxRestSetBuilder<TModel, TKey> builder, Func<TOperationDescriptor, HttpRequestMessage> requestMessageResolver)
+        where TModel : class
+        where TOperationDescriptor : OperationDescriptor
+        => builder.WithEndpoint(requestMessageResolver, HttpMethods.Delete);
+
+    /// <inheritdoc cref="WithEndpoint{TModel,TKey,TOperationDescriptor}(IFluxRestSetBuilder{TModel,TKey},Func{TOperationDescriptor,HttpRequestMessage},HttpMethods)"/>/>
+    public static IFluxRestSetBuilder<TModel, TKey> WithEndpoint<TModel, TKey, TOperationDescriptor>(this IFluxRestSetBuilder<TModel, TKey> builder, Func<TOperationDescriptor, HttpRequestMessage> requestMessageResolver, HttpMethods methods = HttpMethods.All)
+        where TModel : class
+        where TOperationDescriptor : OperationDescriptor
+        => builder.WithEndpoint((TOperationDescriptor operation, IServiceProvider _) => requestMessageResolver.Invoke(operation), methods);
+
+    // ==============================================================
+
     /// <inheritdoc cref="WithEndpoint{TModel,TKey,TOperationDescriptor}(IFluxRestSetBuilder{TModel,TKey},Func{TOperationDescriptor,IServiceProvider,HttpRequestMessage},HttpMethods)"/>
     public static IFluxRestSetBuilder<TModel, TKey> WithGet<TModel, TKey, TOperationDescriptor>(this IFluxRestSetBuilder<TModel, TKey> builder, Func<TOperationDescriptor, IServiceProvider, HttpRequestMessage> requestMessageResolver)
         where TModel : class
@@ -19,6 +59,24 @@ public static class EndpointResolverExtensions
         where TModel : class
         where TOperationDescriptor : OperationDescriptor
         => builder.WithEndpoint(requestMessageResolver, HttpMethods.Post);
+
+    /// <inheritdoc cref="WithEndpoint{TModel,TKey,TOperationDescriptor}(IFluxRestSetBuilder{TModel,TKey},Func{TOperationDescriptor,IServiceProvider,HttpRequestMessage},HttpMethods)"/>
+    public static IFluxRestSetBuilder<TModel, TKey> WithPut<TModel, TKey, TOperationDescriptor>(this IFluxRestSetBuilder<TModel, TKey> builder, Func<TOperationDescriptor, IServiceProvider, HttpRequestMessage> requestMessageResolver)
+        where TModel : class
+        where TOperationDescriptor : OperationDescriptor
+        => builder.WithEndpoint(requestMessageResolver, HttpMethods.Put);
+
+    /// <inheritdoc cref="WithEndpoint{TModel,TKey,TOperationDescriptor}(IFluxRestSetBuilder{TModel,TKey},Func{TOperationDescriptor,IServiceProvider,HttpRequestMessage},HttpMethods)"/>
+    public static IFluxRestSetBuilder<TModel, TKey> WithPatch<TModel, TKey, TOperationDescriptor>(this IFluxRestSetBuilder<TModel, TKey> builder, Func<TOperationDescriptor, IServiceProvider, HttpRequestMessage> requestMessageResolver)
+        where TModel : class
+        where TOperationDescriptor : OperationDescriptor
+        => builder.WithEndpoint(requestMessageResolver, HttpMethods.Patch);
+
+    /// <inheritdoc cref="WithEndpoint{TModel,TKey,TOperationDescriptor}(IFluxRestSetBuilder{TModel,TKey},Func{TOperationDescriptor,IServiceProvider,HttpRequestMessage},HttpMethods)"/>
+    public static IFluxRestSetBuilder<TModel, TKey> WithDelete<TModel, TKey, TOperationDescriptor>(this IFluxRestSetBuilder<TModel, TKey> builder, Func<TOperationDescriptor, IServiceProvider, HttpRequestMessage> requestMessageResolver)
+        where TModel : class
+        where TOperationDescriptor : OperationDescriptor
+        => builder.WithEndpoint(requestMessageResolver, HttpMethods.Delete);
 
     /// <summary>
     /// Configures the <see cref="IFluxRestSetBuilder{TModel, TKey}"/> to use a custom endpoint resolver for the given operation type.
