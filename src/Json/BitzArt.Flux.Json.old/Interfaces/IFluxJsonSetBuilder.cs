c9f9e5117f14d94774a60cbb659aac346322b212
Yuriy Durov <yu.durov@gmail.com>
2025-05-29 12:22:37 +0400

    Remove used

--- src/Json/BitzArt.Flux.Json.old/Interfaces/IFluxJsonSetBuilder.cs
+++
@@ -1,8 +0,0 @@
-namespace BitzArt.Flux;
-
-/// <inheritdoc/>
-public interface IFluxJsonSetBuilder<TModel, TKey> : IFluxJsonServiceBuilder
-    where TModel : class
-{
-    internal IFluxJsonSetOptions<TModel> SetOptions { get; }
-}
