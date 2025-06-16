ccb9ec7bf83c0dc23b7c8463724dcd0698d91b65
N31TH3R <N31TH3R@users.noreply.github.com>
2025-06-16 06:01:43 -0300

    Remove old Flux.Json folder

--- src/Json/BitzArt.Flux.Json.old/Exceptions/FluxJsonFileReadException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BitzArt.Flux;
-
-internal class FluxJsonFileReadException : Exception
-{
-    public FluxJsonFileReadException(string path, Exception innerException)
-        : base($"Error reading JSON from file '{path}'. See inner exception for details", innerException)
-    { }
-}
