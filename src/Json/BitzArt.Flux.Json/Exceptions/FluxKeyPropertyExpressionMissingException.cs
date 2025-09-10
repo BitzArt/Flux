3f6db81f9b54cbc4af09b894fa0736a8a214db3d
N31TH3R <N31TH3R@users.noreply.github.com>
2025-09-10 06:56:23 -0300

    Remove unused exceptions

--- src/Json/BitzArt.Flux.Json/Exceptions/FluxKeyPropertyExpressionMissingException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BitzArt.Flux.Json;
-
-internal class FluxKeyPropertyExpressionMissingException<TModel> : Exception
-{
-    public FluxKeyPropertyExpressionMissingException() : base($"KeyPropertyExpression is required for {typeof(TModel).Name}. Consider using .WithKey() when configuring a Set.")
-    {
-    }
-}
