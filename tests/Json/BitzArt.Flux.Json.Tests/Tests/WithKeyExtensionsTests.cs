77db82ac52602b2ea3d7f556204457fe50a0b71c
N31TH3R <N31TH3R@users.noreply.github.com>
2025-09-10 06:57:32 -0300

    Add WithKeyExtensions tests

---
+++ tests/Json/BitzArt.Flux.Json.Tests/Tests/WithKeyExtensionsTests.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BitzArt.Flux.Json;
+
+public class WithKeyExtensionsTests
+{
+    [Fact]
+    public void WithKey_ValidConditions_GetByIdReturnsResult()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+
+        services.AddFlux(flux =>
+        {
+            flux.AddService("service 1")
+                .UsingJson("Data")
+                    .AddSet<TestModel>()
+                    .FromJsonFile("test-model.set.json")
+                    .WithKey(x => x.Id!.Value);
+        });
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        var setContext = serviceProvider.GetRequiredService<IFluxSetContext<TestModel>>();
+
+        // Act
+        var result = setContext.GetAsync(1);
+
+        // Assert
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public async Task WithKey_WhenNotCalled_GetByIdShouldThrowAsync()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+
+        services.AddFlux(flux =>
+        {
+            flux.AddService("service 1")
+                .UsingJson("Data")
+                    .AddSet<TestModel>()
+                    .FromJsonFile("test-model.set.json");
+        });
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        var setContext = serviceProvider.GetRequiredService<IFluxSetContext<TestModel>>();
+
+        // Act/Assert
+        await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+        {
+            var result = await setContext.GetAsync(1);
+        });
+    }
+
+    [Fact]
+    public void WithKey_WhenItemMissesKeyProperty_ShouldThrow()
+    {
+        var data = """
+        [
+            {
+              "id": 1
+            },
+            {
+              "id": 2
+            },
+            {
+              "name": "Item without id property"
+            }
+        ]
+        """;
+
+        var services = new ServiceCollection();
+
+        // Act/Assert
+        Assert.Throws<InvalidOperationException>(() =>
+        {
+            services.AddFlux(flux =>
+            {
+                flux.AddService("service 1")
+                    .UsingJson()
+                        .AddSet<TestModel>()
+                        .FromJson(data)
+                        .WithKey(x => x.Id!.Value);
+            });
+        });
+    }
+}
