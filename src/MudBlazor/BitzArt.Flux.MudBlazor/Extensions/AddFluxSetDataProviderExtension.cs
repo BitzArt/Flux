4f48d138904cfe17eadf04b91689470f6a1b0c86
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2026-05-20 17:17:47 +0400

    Add AddFluxSetDataProvider<TRequest, TModel>

--- src/MudBlazor/BitzArt.Flux.MudBlazor/Extensions/AddFluxSetDataProviderExtension.cs
+++ src/MudBlazor/BitzArt.Flux.MudBlazor/Extensions/AddFluxSetDataProviderExtension.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BitzArt.Pagination;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BitzArt.Flux.MudBlazor;
 
@@ -19,14 +20,32 @@ public static class AddFluxSetDataProviderExtension
         Func<IServiceProvider, IFluxSetContext<TModel>>? setContextImplementationFactory = null)
         where TModel : class
     {
+        services.AddFluxSetDataProvider<PageResult<TModel, PageRequest>, TModel>(setContextImplementationFactory);
+        return services;
+    }
+
+    /// <summary>
+    /// Adds an <see cref="IFluxSetDataProvider{TRequest, TModel}"/> to the service collection.
+    /// </summary>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TModel"></typeparam>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the provider to</param>
+    /// <param name="setContextImplementationFactory">Can be used to provide a non-default <see cref="IFluxSetContext{TModel}"/></param>
+    /// <returns> <see cref="IServiceCollection"/> to allow chaining </returns>
+    public static IServiceCollection AddFluxSetDataProvider<TRequest, TModel>(
+        this IServiceCollection services,
+        Func<IServiceProvider, IFluxSetContext<TModel>>? setContextImplementationFactory = null)
+        where TRequest : class
+        where TModel : class
+    {
         setContextImplementationFactory ??= (serviceProvider)
             => serviceProvider.GetRequiredService<IFluxSetContext<TModel>>();
 
-        services.AddTransient<FluxSetDataProvider<TModel>>();
+        services.AddTransient<FluxSetDataProvider<TRequest, TModel>>();
 
-        services.AddTransient<IFluxSetDataProvider<TModel>>(serviceProvider =>
+        services.AddTransient<IFluxSetDataProvider<TRequest, TModel>>(serviceProvider =>
         {
-            var provider = serviceProvider.GetRequiredService<FluxSetDataProvider<TModel>>();
+            var provider = serviceProvider.GetRequiredService<FluxSetDataProvider<TRequest, TModel>>();
             var setContext = setContextImplementationFactory!.Invoke(serviceProvider);
             provider.SetContext = setContext;
 
