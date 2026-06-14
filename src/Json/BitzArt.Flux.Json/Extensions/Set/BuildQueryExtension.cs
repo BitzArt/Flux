1dff1d22958386a131c6c7add516a357875aac31
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2026-06-14 11:37:45 +0400

    Add summary

--- src/Json/BitzArt.Flux.Json/Extensions/Set/BuildQueryExtension.cs
+++ src/Json/BitzArt.Flux.Json/Extensions/Set/BuildQueryExtension.cs
@@ -1,7 +1,19 @@
 ﻿namespace BitzArt.Flux.Json;
 
+/// <summary>
+/// Extension methods for configuring the query building logic of a set.
+/// </summary>
 public static class BuildQueryExtension
 {
+    /// <summary>
+    /// Configures the query building logic of a set
+    /// </summary>
+    /// <typeparam name="TModel">The model type of the set</typeparam>
+    /// <param name="builder"></param>
+    /// <param name="buildQuery"></param>
+    /// <returns>
+    /// The <see cref="IFluxJsonSetBuilder{TModel}"/> for further set configuration.
+    /// </returns>
     public static IFluxJsonSetBuilder<TModel> BuildQuery<TModel>(this IFluxJsonSetBuilder<TModel> builder,
         Func<IQueryable<TModel>, OperationDescriptor, IQueryable<TModel>> buildQuery)
         where TModel : class
