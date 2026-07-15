b2e357cc066db9f0438bee1ee09002228ad56bbc
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2026-07-15 09:24:40 +0400

    Add EnrichQueryExtension summary

--- src/Json/BitzArt.Flux.Json/Extensions/Set/EnrichQueryExtension.cs
+++ src/Json/BitzArt.Flux.Json/Extensions/Set/EnrichQueryExtension.cs
@@ -1,19 +1,25 @@
 ﻿namespace BitzArt.Flux.Json;
 
 /// <summary>
-/// Extension methods for enriching the query building logic of a set.
+/// Extension methods for customizing read queries for JSON-backed sets.
 /// </summary>
 public static class EnrichQueryExtension
 {
     /// <summary>
-    /// Configures the query building logic of a set
+    /// Configures a transformation that is applied to the set query before a read operation is resolved.
     /// </summary>
-    /// <typeparam name="TModel">The model type of the set</typeparam>
-    /// <param name="builder"></param>
-    /// <param name="enrichQuery"></param>
-    /// <returns>
-    /// The <see cref="IFluxJsonSetBuilder{TModel}"/> for further set configuration.
-    /// </returns>
+    /// <remarks>
+    /// <para>
+    /// The transformation applies to <see cref="GetOperationDescriptor"/>,
+    /// <see cref="GetAllOperationDescriptor"/>, and <see cref="GetPageOperationDescriptor"/> operations.
+    /// </para>
+    /// </remarks>
+    /// <typeparam name="TModel">Model type stored in the set.</typeparam>
+    /// <param name="builder">Set builder to configure.</param>
+    /// <param name="enrichQuery">
+    /// Transformation that receives the current query and the descriptor for the read operation.
+    /// </param>
+    /// <returns>The <see cref="IFluxJsonSetBuilder{TModel}"/> for further configuration.</returns>
     public static IFluxJsonSetBuilder<TModel> EnrichQuery<TModel>(this IFluxJsonSetBuilder<TModel> builder,
         Func<IQueryable<TModel>, OperationDescriptor, IQueryable<TModel>> enrichQuery)
         where TModel : class
